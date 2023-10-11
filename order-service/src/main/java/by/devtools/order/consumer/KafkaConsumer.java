8716c2718f68fb9f6d72fda2593a1753b01b6460
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-10-11 13:34:25 +0300

    feat: complete kafka consumer in order service

--- order-service/src/main/java/by/devtools/order/consumer/KafkaConsumer.java
+++ order-service/src/main/java/by/devtools/order/consumer/KafkaConsumer.java
@@ -9,15 +9,18 @@ import by.devtools.order.util.JsonUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Component
+@Transactional
 @RequiredArgsConstructor
 public class KafkaConsumer {
 
     private final OrderRepository orderRepository;
-    //private final KafkaTemplate<Integer, String> kafkaTemplate;
+    private final KafkaTemplate<Integer, String> kafkaTemplate;
 
     @KafkaListener(topics = "order-status-topic", groupId = "order-group")
     public void onOrderStatusEventReceived(String event) {
@@ -25,22 +28,24 @@ public class KafkaConsumer {
         log.info("Received status event: {}", statusEvent);
         Order currentOrder = orderRepository.findById(statusEvent.orderId())
                 .orElseThrow(() -> new OrderNotFoundException("Order not found. id = " + statusEvent.orderId()));
+        String newStatus = null;
         switch (statusEvent.status()) {
             case Statuses.REJECTED -> {
-                log.info("Process rejected status...");
-                // stop global transaction
-                // send message to rollback local transactions
-                // set status CANCELLED
+                log.info("Status event = rejected");
+                kafkaTemplate.send("rollback-topic", JsonUtil.toJson(currentOrder));
+                newStatus = Statuses.CANCELLED;
             }
             case Statuses.ACCEPTED -> {
-                log.info("Processing accepted status...");
+                log.info("Status event = accepted");
                 if (Statuses.NEW.equals(currentOrder.getStatus())) {
-                    // if order status is NEW just update status to WAITING in current order
-                }
-                if (Statuses.WAITING.equals(currentOrder.getStatus())) {
-                    // if order status is WAITING - set status order CONFIRMED
+                    newStatus = Statuses.WAITING;
+                } else if (Statuses.WAITING.equals(currentOrder.getStatus())) {
+                    newStatus = Statuses.CONFIRMED;
                 }
             }
         }
+        currentOrder.setStatus(newStatus);
+        log.info("Updating order with id={} newStatus={}", currentOrder.getId(), newStatus);
+        orderRepository.save(currentOrder);
     }
 }
