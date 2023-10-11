b536f717222afe6555a2db627ae0e4b98b82ed8e
Kiryl Pryhozhy <kirill19962003@gmail.com>
2023-10-11 13:42:46 +0300

    chore: init inventory service

---
+++ inventory-service/src/test/java/by/devtools/inventory/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package by.devtools.inventory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
